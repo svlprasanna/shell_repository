d29c4a816ddf23f06749870dfbecddff6c51a48c
svlprasanna <svlprasanna92@gmail.com>
2024-05-21 16:42:19 +0530

    07.arrays script

--- 07.arrays.sh
+++ 07.arrays.sh
@@ -2,6 +2,6 @@
 
 FRUITS=("APPLE" "BANANA" "WATERMELON")
 
-echo "first fruits is $FRUITS[0]"
-echo "first fruits is $FRUITS[1]"
-echo "first fruits is $FRUITS[2]"
+echo "first fruits is ${FRUITS[0]}"
+echo "first fruits is ${FRUITS[1]}"
+echo "first fruits is ${FRUITS[2]}"
