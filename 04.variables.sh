71222f1379c20703675c6011d127145b6492ac6c
svlprasanna <svlprasanna92@gmail.com>
2024-05-21 15:39:18 +0530

    04.variables script

---
+++ 04.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hello $PERSON2, good morning"
+echo "$PERSON2::hey $PERSON1, very good morning"
+echo "$PERSON1:: how are you $PERSON2?"
+echo "$PERSON2:: am doing great, how about you?"
