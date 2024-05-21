eda3b2c1ce544275494670fb561b449a82207586
svlprasanna <svlprasanna92@gmail.com>
2024-05-21 12:57:37 +0530

    variables script

---
+++ 02.variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: hello suresh, good morning"
+echo "suresh::hey ramesh, very good morning"
+echo "ramesh:: how are you ramesh?"
+echo "suresh:: am doing great, how about you?"
